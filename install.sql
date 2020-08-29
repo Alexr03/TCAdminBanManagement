09d89313db6c72fccf7365ff0f3ed39b1dcb8e95
Alex Redding <Alexredding180@gmail.com>
2020-08-29 19:04:34 +0100

    Fix module commands

--- install.sql
+++ install.sql
@@ -38,9 +38,9 @@ VALUES (2, '918250d5-e6d5-4398-9434-44b0a17cd231', null, null, null, '/BanManage
         'MenuIcons/Base/ServerComponents24x24.png', 0, 1000, null, null, null, null, null);
 
 INSERT INTO tc_site_map (page_id, module_id, parent_page_id, parent_page_module_id, category_id, url, mvc_url,
-                                 controller, action, display_name, page_small_icon, panelbar_icon, show_in_sidebar,
-                                 view_order, required_permissions, menu_required_permissions, page_manager,
-                                 page_search_provider, cache_name)
+                         controller, action, display_name, page_small_icon, panelbar_icon, show_in_sidebar,
+                         view_order, required_permissions, menu_required_permissions, page_manager,
+                         page_search_provider, cache_name)
 VALUES (3, '918250d5-e6d5-4398-9434-44b0a17cd231', 40, '07405876-e8c2-4b24-a774-4ef57f596384', 1,
         '/BanManagement/AutomatedBans', '/BanManagement/AutomatedBans', 'BanManagement', 'AutomatedBans',
         'Manage Automatic Bans', 'MenuIcons/Base/ServerComponents24x24.png', 'MenuIcons/Base/ServerComponents16x16.png',
@@ -77,3 +77,25 @@ INSERT INTO tc_panelbar_categories (category_id, module_id, display_name, view_o
                                     parent_module_id, page_id, panelbar_icon)
 VALUES (1, '918250d5-e6d5-4398-9434-44b0a17cd231', 'Ban Management', 1000, 6, '07405876-e8c2-4b24-a774-4ef57f596384',
         null, null);
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+INSERT INTO tc_module_commands (command_id, module_id, is_custom, description, sender_class, command_name,
+                                command_class, execute_order, enabled, can_disable, master_only)
+VALUES (1, '918250d5-e6d5-4398-9434-44b0a17cd231', 1, 'Deny access to Panel Login if banned',
+        'TCAdmin.SDK.Objects.User', 'PanelLogin', 'TCAdminBanManagement.Events.LoginProtection, TCAdminBanManagement',
+        100, 1, 0, null);
+INSERT INTO tc_module_commands (command_id, module_id, is_custom, description, sender_class, command_name,
+                                command_class, execute_order, enabled, can_disable, master_only)
+VALUES (3, '918250d5-e6d5-4398-9434-44b0a17cd231', 1, 'When a banned user attempts to access the panel or ftp',
+        'TCAdminBanManagement.Models.Objects.BannedIp', 'AttemptedAccess',
+        'TCAdminBanManagement.Events.BannedIpEvent, TCAdminBanManagement', 100, 1, 0, null);
+INSERT INTO tc_module_commands (command_id, module_id, is_custom, description, sender_class, command_name,
+                                command_class, execute_order, enabled, can_disable, master_only)
+VALUES (2, '918250d5-e6d5-4398-9434-44b0a17cd231', 1, 'Deny access to FTP if banned', 'TCAdmin.SDK.Objects.User',
+        'FtpLogin', 'TCAdminBanManagement.Events.LoginProtection, TCAdminBanManagement', 100, 1, 0, null);
+INSERT INTO tc_module_commands (command_id, module_id, is_custom, description, sender_class, command_name,
+                                command_class, execute_order, enabled, can_disable, master_only)
+VALUES (4, '918250d5-e6d5-4398-9434-44b0a17cd231', 1, 'Failed Login Attempts Automatic Banning',
+        'TCAdminBanManagement.Models.Objects.BannedIp', 'FailedLogin',
+        'TCAdminBanManagement.Events.BannedIpEvent, TCAdminBanManagement', 100, 1, 0, null);
--- uninstall.sql
+++ uninstall.sql
@@ -5,3 +5,4 @@ DELETE FROM tc_site_map WHERE module_id LIKE '918250d5-e6d5-4398-9434-44b0a17cd2
 DELETE FROM tc_permission_categories WHERE module_id LIKE '918250d5-e6d5-4398-9434-44b0a17cd231';
 DELETE FROM tc_permissions WHERE module_id LIKE '918250d5-e6d5-4398-9434-44b0a17cd231';
 DELETE FROM tc_panelbar_categories WHERE module_id LIKE '918250d5-e6d5-4398-9434-44b0a17cd231';
+DELETE FROM tc_module_commands WHERE module_id LIKE '918250d5-e6d5-4398-9434-44b0a17cd231';
